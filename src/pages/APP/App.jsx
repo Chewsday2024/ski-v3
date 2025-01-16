e1a7f2dc9ed53e5a79561619f6d532d8a316be02
Chewsday <z647623@gmail.com>
2025-01-16 16:44:04 +0800

    react專案 公版V3

---
+++ src/pages/APP/App.jsx
@@ -0,0 +1,20 @@
+import './App.scss';
+
+
+
+import SkiHouse from '../SkiHouse/SkiHouse';          <SkiHouse />
+import Coach from '../Coach/Coach';                   <Coach />
+import Order from '../Order/Order';                   <Order />
+
+
+function App() {
+  
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
