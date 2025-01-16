dcc3842213473ccf4200e2993b07070b0a7afaa4
Chewsday <z647623@gmail.com>
2025-01-16 16:47:19 +0800

    微修改

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/ski/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/ski-v3/' : '/',
   plugins: [react()],
 })
